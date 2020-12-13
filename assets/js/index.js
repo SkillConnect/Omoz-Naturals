9ab196478307081895347a8409fbf7de1ee38cbd
Shubham Bisht <shubham99bisht@gmail.com>
2020-12-14 00:38:06 +0530

    Initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,38 @@
+
+!(function($) {
+  "use strict";
+  // jQuery counterUp
+  $('[data-toggle="counter-up"]').counterUp({
+    delay: 10,
+    time: 1000
+  });
+
+  // Porfolio isotope and filter
+  $(window).on('load', function() {
+    var portfolioIsotope = $('.portfolio-container').isotope({
+      itemSelector: '.portfolio-item'
+    });
+
+    $('#portfolio-flters li').on('click', function() {
+      $("#portfolio-flters li").removeClass('filter-active');
+      $(this).addClass('filter-active');
+
+      portfolioIsotope.isotope({
+        filter: $(this).data('filter')
+      });
+      aos_init();
+    });
+
+    // Initiate venobox (lightbox feature used in portofilo)
+    $(document).ready(function() {
+      $('.venobox').venobox();
+      $('.venobox_custom').venobox({
+        framewidth : '400px', 
+        frameheight: '400px'
+    });
+    });
+  });
+})(jQuery);
+
+
+// document.getElementById("hero").style.background = 'url("./assets/img/pics/1.jpg")'
---
+++ assets/js/loader.js
@@ -0,0 +1,63 @@
+// Function to insert values in String
+String.prototype.format = function () {
+    var i = 0, args = arguments;
+    return this.replace(/{}/g, function () {
+      return typeof args[i] != 'undefined' ? args[i++] : '';
+    });
+  };
+
+try{  
+  // URL example: /product.html?id=product_name
+  params = window.location.search.split("id=")[1]
+  id = params.split("&")[0]
+  
+  path = "/products/{}.json".format(id)
+
+  $.ajax({
+    url: path,
+    dataType: "json",
+    success: (data) => loadContents(data)
+    })
+} catch (e) {
+    console.log("Error report: ", e)
+} 
+
+function loadContents(data){
+  console.log(data)
+  var title = data["title"];
+  var description = data["description"];
+  var images = data["images"];
+  var advantages = decorate(data["advantages"].join(" "));
+  var how = data["how-to-use"].join(" ");
+
+  document.getElementById("title").innerHTML += `${title}`
+  document.getElementById("description").innerHTML = `${description}`
+  document.getElementById("advantages").innerHTML = `${advantages}`
+  document.getElementById("how-to-use").innerHTML = `${how}`
+  document.getElementById("images").innerHTML = createImages(images)
+  document.getElementById("defaultImage").style = "display:none"
+
+  // Portfolio details carousel
+  $(".owl-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    items: 1
+  });
+}
+
+function decorate(inputString) {
+  return inputString.replaceAll("(-)", "<i class='bx bx-check'></i>")
+}
+
+function createImages(imagesArray) {
+  const base = '<img src="{}" class="img-fluid"/>';
+  var imgs = ""
+  imagesArray.forEach(imageSrc => {
+    var img = base.format(imageSrc);
+    imgs += img
+  });
+  console.log(imgs);
+  return imgs
+}
+
