a2c972bb1ff19db28ed53f9af73e2e4b9c831993
Shubham Bisht <shubham99bisht@gmail.com>
2021-01-03 16:08:29 +0530

    Testimonial slider 30

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,7 +10,7 @@
   // Testimonials carousel (uses the Owl Carousel library)
   $(".testimonials-carousel").owlCarousel({
     autoplay: true,
-    autoplayTimeout: 60000,
+    autoplayTimeout: 30000,
     autoplayHoverPause: true,
     dots: true,
     loop: true,
