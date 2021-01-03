3c6be41b26321513d2305e2f998708e431bd393c
Shubham Bisht <shubham99bisht@gmail.com>
2021-01-03 12:26:55 +0530

    Fine tune 2

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,22 +7,14 @@
     time: 1000
   });
 
-  // Testimonials carousel (uses the Owl Carousel library)
-  $(".testimonials-carousel").owlCarousel({
-    autoplay: true,
-    autoplayTimeout: 10000,
-    dots: true,
-    loop: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      768: {
-        items: 2
-      },
-      900: {
-        items: 3
-      }
-    }
-  });
 })(jQuery);
+
+
+setInterval(function() {
+  var src = document.getElementById("sliderImage").src
+  var number = parseInt(src.split("/d")[1].split(".jpg")[0])
+  if(number!=5) number+=1
+  else number = 1
+
+  document.getElementById("sliderImage").src = `./assets/img/slide/d${number}.jpg`
+}, 6000)
