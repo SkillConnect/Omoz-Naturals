66d8f2aa514d9944e1832aeab9cc2eb0fbf760a1
Shubham Bisht <shubham99bisht@gmail.com>
2021-01-03 15:42:43 +0530

    fine tune 3

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,6 +7,25 @@
     time: 1000
   });
 
+  // Testimonials carousel (uses the Owl Carousel library)
+  $(".testimonials-carousel").owlCarousel({
+    autoplay: true,
+    autoplayTimeout: 60000,
+    autoplayHoverPause: true,
+    dots: true,
+    loop: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 2
+      },
+      900: {
+        items: 3
+      }
+    }
+  });
 })(jQuery);
 
 
