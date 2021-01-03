5c2d39dc1090551c538c4b8afddfc2b6a1efe4ca
Shubham Bisht <shubham99bisht@gmail.com>
2021-01-03 17:36:45 +0530

    fine tune again :(

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,7 +10,7 @@
   // Testimonials carousel (uses the Owl Carousel library)
   $(".testimonials-carousel").owlCarousel({
     autoplay: true,
-    autoplayTimeout: 30000,
+    autoplayTimeout: 22000,
     autoplayHoverPause: true,
     dots: true,
     loop: true,
